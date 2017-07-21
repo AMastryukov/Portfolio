66cd9525d971a3a4cc94276c492c649428c8f88c
Alexander Mastryukov <alexandermastryukov@gmail.com>
2017-07-21 17:20:38 -0400

    Added image uploading for posts and projects using paperclip

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,15 @@
 class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all.order('created_at DESC')
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    @projects = Project.order("created_at desc").limit(4).offset(1)
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:title,:body)
+  end
 end
